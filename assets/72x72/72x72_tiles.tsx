51fc12334291a5322133b02cc6c0527e174f9229
Marcos Diaz <marcos.dure.diaz@gmail.com>
2020-10-14 22:36:16 -0300

    + assets

---
+++ assets/72x72/72x72_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="72x72_tiles" tilewidth="72" tileheight="72" tilecount="300" columns="6">
+ <image source="72x72_tiles.png" trans="ff00ff" width="432" height="3600"/>
+</tileset>
