d014807133f184dd665abf96ecee4677fc56b093
Marcos Diaz <marcos.dure.diaz@gmail.com>
2020-10-21 17:11:17 -0300

    Integracao CMakDoxygen funcionando

---
+++ build/CMakeFiles/EngrenagemDeMetalSolida.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "html"
+  "CMakeFiles/EngrenagemDeMetalSolida.dir/main.cpp.o"
+  "EngrenagemDeMetalSolida.pdb"
+  "EngrenagemDeMetalSolida"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EngrenagemDeMetalSolida.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
